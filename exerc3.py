18133ae2a10bc22475f88fb659172211d224b4cf
Jardel Bastos <84300151+realidademax@users.noreply.github.com>
2023-05-05 00:50:47 -0300

    first commit

---
+++ exerc3.py
@@ -0,0 +1,4 @@
+valor1 = input("Digite por gentileza um valor para efetuar soma: ")
+valor2 = input("Digite o segundo valor para efetuar soma: ")
+soma = int(valor1) + int(valor2)
+print(f"A soma dos valores digitados foi: {soma}")
